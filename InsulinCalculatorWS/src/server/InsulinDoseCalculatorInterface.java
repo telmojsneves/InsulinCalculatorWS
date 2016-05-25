deed2a00620ae9e06dd18700396c999a03291f60
Telmo Neves <telmo.jsneves@gmail.com>
2016-05-26 00:52:05 +0100

    set the right version

---
+++ InsulinCalculatorWS/src/server/InsulinDoseCalculatorInterface.java
@@ -0,0 +1,25 @@
+package server;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
+public interface InsulinDoseCalculatorInterface {
+
+	
+	@WebMethod 
+	int mealtimeInsulinDose(int carbohydrateAmount,
+										int carbohydrateToInsulinRatio,
+										int preMealBloodSugar,
+										int targetBloodSugar,
+										int personalSensitivity);
+	
+	@WebMethod
+	int backgroundInsulinDose(int bodyWeight);
+	
+	@WebMethod
+	int personalSensitivityToInsulin(int physicalActivityLevel, int[] physicalActivitySamples, int[] bloodSugarDropSamples);
+	
+
+	
+}
